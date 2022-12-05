e90d5751c2937a4c26a8e4c4f5bc48f46577bc48
JorgeMioska <quispechilojorge@gmail.com>
2022-12-05 05:25:59 -0500

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const $boxes=document.querySelectorAll(".boxEffect");
 window.addEventListener("scroll", ()=>{
-    const triggerBtn=window.innerHeight/5*4;
+    const triggerBtn=window.innerHeight/5*4;//not to become 0, we want a higher height to the effect so it starts
     $boxes.forEach(box=>{
         const boxTop=box.getBoundingClientRect().top;//gets the box properties related to the x-y axis to the window
         if (boxTop<triggerBtn){//scroll will reduce the distance btw the window and the box 
